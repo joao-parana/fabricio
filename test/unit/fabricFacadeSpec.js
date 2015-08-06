46751b5af2c926ace7ebc39960a22fbf1b3016d9
Diego Martins <diegomartins.br@gmail.com>
2015-08-05 22:58:18 -0300

    Changing from "Mocha" to "Jasmine" for unit testing. Creating a better test folder structure

---
+++ test/unit/fabricFacadeSpec.js
@@ -0,0 +1,30 @@
+var fabricFacade = require('../../app/fabricFacade');
+
+describe("FabricFacade", function() {
+
+  describe("listCommands", function() {
+
+      it("should return ['foo']", function() {
+
+        var commandList = fabricFacade.listCommands("test/unit/fabfile-foo.py");
+
+        expect(commandList.length).toEqual(1);
+        expect(commandList[0]).toEqual("foo");
+      });
+
+      it("should return ['bar', 'foo']", function() {
+
+        var commandList = fabricFacade.listCommands("test/unit/fabfile-foobar.py");
+
+        expect(commandList.length).toEqual(2);
+        expect(commandList[0]).toEqual("bar");
+        expect(commandList[1]).toEqual("foo");
+
+      });
+  });
+
+  it("should check that fabricFacade is not undefined", function() {
+    expect(fabricFacade).not.toBe(undefined);
+  });
+
+});

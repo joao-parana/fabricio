134482887b263d5df0e47e329a53de551dce3faf
raphaelpaiva <raphaeldpaiva@gmail.com>
2015-08-12 18:46:39 -0300

    Making the deploy script remove the old container and sensible to build number

--- fabric/fabfile-fabricio.py
+++ fabric/fabfile-fabricio.py
@@ -8,16 +8,17 @@ fabfile_dir = fabricio_work_dir + "/fabfile"
 
 dockerfile_path = fabricio_work_dir + "/Dockerfile"
 
-last_successful_build_url = "http://localhost:8080/job/fabricio/lastSuccessfulBuild/artifact/*zip*/archive.zip"
 archive_zip_path = "/tmp/archive.zip"
 
 @task
-def deploy():
+def deploy(build_number='lastSuccessfulBuild'):
     cleanup()
 
-    download_latest()
+    download_latest(build_number)
 
     prepare_fabfile()
+
+    remove_old_container_if_necessary()
 
     run_docker_container()
 
@@ -27,8 +28,10 @@ def cleanup():
     run("mkdir " + fabricio_work_dir)
     run("mkdir " + fabfile_dir)
 
-def download_latest():
-    run("wget " + last_successful_build_url + " -O " + archive_zip_path)
+def download_latest(build_number):
+    build_url = "http://localhost:8080/job/fabricio/" + build_number + "/artifact/*zip*/archive.zip"
+    
+    run("wget " + build_url + " -O " + archive_zip_path)
     run("unzip " + archive_zip_path + " 'archive/*' -d " + fabricio_unzip_dir)
     run("mv /tmp/fabricio-unzip/archive/ " + fabricio_app_dir)
 
@@ -39,3 +42,11 @@ def run_docker_container():
     run("echo 'FROM fabricio/fabricio-runtime' >> " + dockerfile_path)
     run("cd " + fabricio_work_dir + "&& docker build -t fabricio/app .")
     run("docker run -d -p 1337:1337 --name fabricio fabricio/app")
+
+def remove_old_container_if_necessary():
+    with settings(warn_only=True):
+        result = run("docker ps -a | grep fabricio")
+        if result.return_code != 0:
+            pass
+        else:
+            run("docker rm -f fabricio")
